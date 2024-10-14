4f35e41d0982123bf4344641863e6e0a85b9b677
MaxDatii <mxgreek@gmail.com>
2024-10-14 14:59:43 -0400

    switch to array; add "invert" variable to manipelate the invertion in the future.

--- 01-pyramid-generator/script.js
+++ 01-pyramid-generator/script.js
@@ -1,19 +1,29 @@
-const character = "#";
+const character = "!";
 const count = 8;
 const rows = [];
+let inverted = false; //change to invert the pyramid
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+//examples with different type of loops
 /*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
 }*/
 
-while (rows.length < count) {
+//while loop
+/*while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
-}
+}*/
 
 //loop to invert the pyramid
 /*for (let i = count; i > 0; i--) {
