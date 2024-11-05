dea0d5a5c27cac488510f42ffb68c541dcb8d835
MaxDatii <mxgreek@gmail.com>
2024-11-05 10:34:17 -0500

    add more functions including "update location" function to avoid code repetition.

--- 03-role-playing-game/script.js
+++ 03-role-playing-game/script.js
@@ -17,10 +17,25 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function update(location) {
+
+}
+
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
 
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
@@ -38,4 +53,12 @@ function goCave() {
 
 function fightDragon() {
   console.log("Fighting dragon.");
+}
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
 }
