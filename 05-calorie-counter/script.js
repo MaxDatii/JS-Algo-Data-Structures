c1a8519d30552d6afc46844959d6d0cbccb7d39f
MaxDatii <mxgreek@gmail.com>
2024-11-14 09:08:45 -0500

    add functionality to cleanInputString function; create function to detect scientific notation like "1e3" and return the error.

--- 05-calorie-counter/script.js
+++ 05-calorie-counter/script.js
@@ -8,5 +8,11 @@ let isError = false;
 
 function cleanInputString(str) {
   const regex = /[+-\s]/g;
-
+  return str.replace(regex, '');
 }
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+console.log(isInvalidInput("1e3"));
