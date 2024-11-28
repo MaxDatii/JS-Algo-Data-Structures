39ce3dcf476bc9fbd119c2679009c5c1419a6861
MaxDatii <mxgreek@gmail.com>
2024-11-28 07:23:47 -0500

    create suffle function ti shuffle the songs; use Math.random() - 0.5 method to randomize the array; reset the values of currentSong and currentSongtime when button is pressed;

--- 07-music-player/script.js
+++ 07-music-player/script.js
@@ -100,6 +100,7 @@ const playSong = (id) => {
 
   highlightCurrentSong();
   setPlayerDisplay();
+  setPlayButtonAccessibleText();
   audio.play();
 };
 
@@ -129,6 +130,17 @@ const playPreviousSong = () => {
 
     playSong(previousSong.id);
    }
+};
+
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
 };
 
 const setPlayerDisplay = () => {
@@ -200,6 +212,8 @@ pauseButton.addEventListener("click",  pauseSong);
 nextButton.addEventListener("click", playNextSong);
 
 previousButton.addEventListener("click", playPreviousSong);
+
+
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
