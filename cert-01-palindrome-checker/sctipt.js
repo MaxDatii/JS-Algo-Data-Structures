2392c66d2d9170c597975393d979748ead6f4f19
MaxDatii <mxgreek@gmail.com>
2024-12-04 19:13:52 -0500

    add const for the main html elements

---
+++ cert-01-palindrome-checker/sctipt.js
@@ -0,0 +1,5 @@
+// Get references to HTML elements
+const textInput = document.getElementById(text-input);
+const checkButton = document.getElementById(check-btn);
+const resultDiv = documsnt.getElementById(result);
+
