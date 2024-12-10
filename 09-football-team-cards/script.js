27bb55e11fdc98df12e6495475e0471ec4227541
MaxDatii <mxgreek@gmail.com>
2024-12-09 22:43:59 -0500

    add logic to return special mark if player is teams captain, display all player paramenters including nickname if he has one.

--- 09-football-team-cards/script.js
+++ 09-football-team-cards/script.js
@@ -187,11 +187,14 @@ const setPlayerCards = (arr = players) => {
     ({ name, position, number, isCaptain, nickname }) => {
      return `
         <div class="player-card">
-          <h2>${name}</h2>
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
       `;
     }
-  );
+  )
 
 
 };
