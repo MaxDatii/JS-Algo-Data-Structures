163b9e2399200da27a0206355918a3903659380e
MaxDatii <mxgreek@gmail.com>
2024-12-11 17:44:37 -0500

    add a switch statement which will check for the user's selection from the player dropdown menu and filter out cards based on the player's positions.

--- 09-football-team-cards/script.js
+++ 09-football-team-cards/script.js
@@ -197,9 +197,12 @@ const setPlayerCards = (arr = players) => {
 };
 
 playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+  
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
 
-
-console.log(e.target.value)
-
-
+  }
 });
